43ba80e79aa71d8bd8c6d8039d6adc572176c133
Savannah Younts <savyounts@gmail.com>
2018-09-20 19:13:59 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920190103) do
+ActiveRecord::Schema.define(version: 20180920191349) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -37,9 +37,10 @@ ActiveRecord::Schema.define(version: 20180920190103) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "category_id"
+    t.string   "published_status"
   end
 
 end
