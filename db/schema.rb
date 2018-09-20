38d0efb8435c56f5c30a8f499bbcf5330145af51
Savannah Younts <savyounts@gmail.com>
2018-09-20 19:12:46 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920184550) do
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "genre"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180920184041) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
